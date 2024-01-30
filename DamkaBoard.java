5d1e3b8e55599cbc2c3c339258bd1b0bc17cd3e9
NoamHasson <noam.hasson01@post.runi.ac.il>
2024-01-30 18:36:06 +0200

    6

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -6,15 +6,17 @@ public class DamkaBoard {
 		
 		int n = Integer.parseInt(args[0]); 
 
-		for(int i=1;i<=n;i++) {
+		for(int i=0;i<n;i++) {
 			for(int j=0;j<n;j++) {
+				if (i%2==0) {
+					System.out.print("*" + " ");
+				} else {
 				
-				System.out.print("*" + " ");
+				System.out.print(" " + "*");
+				}
 			}
 			System.out.println();
-			if (i%2==1) {
-				System.out.print(" ");
-			}
+		
 			
 		} 
 	}	
